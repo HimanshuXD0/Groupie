6d46d3f8bd1345a16fb57b84158508b865b76de4
HimanshuXD0 <112165555+HimanshuXD0@users.noreply.github.com>
2022-09-30 21:03:04 +0530

    Update constant.dart

--- lib/Shared/constant.dart
+++ lib/Shared/constant.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static String apiKey = "AIzaSyD1hTfQ8BZF2PTiJFO3rb265T3zlQwNGPk";
+  static String apiKey = "";
   static String appId = "1:392762566545:web:4ce7909644d9febb561c30";
   static String messagingSenderId = "392762566545";
   static String projectId = "groupie-c5f8e";
